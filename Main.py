ba7f8c3a3366832e325fa0e37772a1af3401b823
whoever11 <cjthesedays000@gmail.com>
2024-03-18 13:42:28 -0400

    5 is done

--- Main.py
+++ Main.py
@@ -88,7 +88,7 @@ statuses = {
     "Ringo": "offline"
 }
 
-print(f"number of people online: {online_count(statuses)}")
+#print(f"number of people online: {online_count(statuses)}")
 # ---------------------------------
 #      Solution Goes Here ->
 # ---------------------------------
@@ -98,6 +98,13 @@ print(f"number of people online: {online_count(statuses)}")
 #  5) Give me the discount
 # Create a function in Python that accepts two parameters. The first should be the full price of an item as an integer. The second should be the discount percentage as an integer.
 # The function should return the price of the item after the discount has been applied. For example, if the price is 100 and the discount is 20, the function should return 80.
+
+def apply_discount(price, discount_percentage):
+    discount_amount = (price * discount_percentage) / 100
+    final_price = price - discount_amount
+    return final_price
+
+print(apply_discount(100, 20))
 
 # ---------------------------------
 #      Solution Goes Here ->
