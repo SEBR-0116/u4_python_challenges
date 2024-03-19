6f50c03c3bcd9236b285ad1c6237004a66336643
joy <67973116+aquaj0y@users.noreply.github.com>
2024-03-18 21:30:32 -0500

    hw submit

--- Main.py
+++ Main.py
@@ -18,7 +18,16 @@
 #      Solution Goes Here ->
 # ---------------------------------
 
+def convert_to_seconds(unit):
+  seconds = unit * 60;
+  hours = unit * 3600;
+  days = hours * 24;
+  month_of_june = days * 30;
+  month_of_august = days * 31;
+  # print(seconds)
+  # print(month_of_june)
 
+# convert_to_seconds(1)
 
 #  2) Middle letter
 
@@ -30,6 +39,14 @@
 #      Solution Goes Here ->
 # ---------------------------------
 
+def mid(str):
+  if len(str) % 2 == 0:
+    print('')
+  else: 
+    middle = int(len(str)/2)
+    print(str[middle])
+
+# mid('abc')
 
 # ### 3) Hide the credit card number
 # Write a function in Python that accepts a credit card number. It should return a string where all the characters are hidden with an asterisk except the last four. For example, if the function gets sent "1234567894444", then it should return "*********4444".
@@ -39,6 +56,13 @@
 #      Solution Goes Here ->
 # ---------------------------------
 
+def show_last_four(num):
+  last_four = num[-4:]
+  replace_num = len(num[:-4])
+  num = num.replace(num[:-4], "*" * replace_num)
+  print (num)
+
+# show_last_four('123456789')
 
 
 # ### 4) Online status
@@ -47,12 +71,12 @@
 # For example, consider the following dictionary:
 
 # ```
-# statuses = {
-#     "John": "online",
-#     "Paul": "offline",
-#     "George": "online",
-#     "Ringo": "offline"
-# }
+statuses = {
+    "John": "online",
+    "Paul": "offline",
+    "George": "online",
+    "Ringo": "offline"
+}
 
 # ```
 
@@ -65,7 +89,16 @@
 #      Solution Goes Here ->
 # ---------------------------------
 
-
+def online_count(dict):
+  # take in a dictionary 
+  count = 0
+  for el in dict.values():
+    if el == 'online':
+      count += 1
+  # return num of ppl online
+  print(count)
+  
+# online_count(statuses)
 
 #  5) Give me the discount
 # Create a function in Python that accepts two parameters. The first should be the full price of an item as an integer. The second should be the discount percentage as an integer.
@@ -75,6 +108,13 @@
 #      Solution Goes Here ->
 # ---------------------------------
 
+def discount_price(full, disc_percentage):
+  # return 
+  # 100 - 100 * 20%
+  new_price = full - full * disc_percentage * 0.01
+  print(new_price)
+
+# discount_price(100, 30)
 
 #  6) Pythagorean Theorum
 
@@ -85,7 +125,11 @@
 # ---------------------------------
 #      Solution Goes Here ->
 # ---------------------------------
+import math
+def hypotenouse(opposite, adjacent):
+  print(math.hypot(opposite, adjacent))
 
+# hypotenouse(2, 2)
 
 #  7) Fibonacci Sequence 
 # Everyone's favorite Math Problem!
@@ -93,8 +137,38 @@
 # The Fibonacci numbers are the numbers in the following integer sequence.
 # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
 # In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the recurrence relation between two adjacent steps in a list
-# Create a python function that takes two numbers and finds the next Nine intervals using the Fibonacci Sequence
+# Create a python function that takes one number and finds the next Nine intervals using the Fibonacci Sequence
 
 # ---------------------------------
 #      Solution Goes Here ->
 # ---------------------------------
+
+def fibonacci(num1, num2):
+  count = 0
+  while count < 9:
+    count += 1
+    print(num1, num2)
+    num3 = num1 + num2
+    print(num3)
+    num1 = num2
+    num2 = num3
+
+
+
+  # count = 0
+  # num_arr = []
+
+  # while count<9:
+  #   while num < 3 and 0 <= num:
+  #     num_arr.append(num)
+  #     print(num)
+  #     num +=1
+  #     count += 1
+  
+  #   count += 1
+
+  #   num = (num-1) + (num-2)
+  #   print('count is',count)
+  #   print(num)
+
+fibonacci(3, 5)
